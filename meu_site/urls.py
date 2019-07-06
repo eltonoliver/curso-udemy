5649a50521eb7b28fd7e5e5fe18bd1c29df6c29b
Elton Oliveira <eltonoliveirape@gmail.com>
2019-07-06 15:33:06 -0400

    Cadastro de usuário

--- meu_site/urls.py
+++ meu_site/urls.py
@@ -22,6 +22,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('django.contrib.auth.urls')),
+    path('accounts/', include('accounts.urls')),
     path('', include('blog.urls')),
 ]
 if settings.DEBUG:
