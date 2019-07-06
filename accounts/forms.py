85d2a139db24bbd2cab93a21ad88895cfcdf6b3b
Elton Oliveira <eltonoliveirape@gmail.com>
2019-07-06 18:33:00 -0400

    Inserido um campo e-mail obrigatório

---
+++ accounts/forms.py
@@ -0,0 +1,16 @@
+from django import forms
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+
+class UserCreationFormWithEmail(UserCreationForm):
+    email = forms.EmailField(required=True, help_text="Obrigatório, digite um e-mail válido")
+
+    class Meta:
+        model = User
+        fields = ("username","email")
+
+    def clean_email(self):
+        email = self.cleaned_data.get("email")   
+        if User.objects.filter(email=email).exists():
+            raise forms.ValidationError("Este e-mail já está cadastrado, por favor utilize outro.") 
+        return email    
--- accounts/views.py
+++ accounts/views.py
@@ -2,9 +2,10 @@ from django.shortcuts import render
 from django.contrib.auth.forms import UserCreationForm
 from django.urls import reverse_lazy
 from django.views import generic
+from .forms import UserCreationFormWithEmail
 # Create your views here.
 
 class SignUpView(generic.CreateView):
-    form_class = UserCreationForm
+    form_class = UserCreationFormWithEmail
     success_url = reverse_lazy('login')
     template_name = 'accounts/signup.html'
